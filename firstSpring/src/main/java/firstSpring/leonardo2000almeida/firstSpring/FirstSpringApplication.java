3fbff32a2754720e5277e8aa7eb83565fa117f79
Leonardo Almeida <leonardo.oliveira@contazap.com>
2021-06-03 21:07:42 -0300

    Initial commit

---
+++ firstSpring/src/main/java/firstSpring/leonardo2000almeida/firstSpring/FirstSpringApplication.java
@@ -0,0 +1,13 @@
+package firstSpring.leonardo2000almeida.firstSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringApplication.class, args);
+	}
+
+}
